bd6141102162de22648bb3593ac37b582a1df9b0
ajaygaur <akg23245@gmail.com>
2021-09-23 12:34:48 +0530

    code update

---
+++ apps/callreport/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'citi.cpb.frontend-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'callreport';
+}
